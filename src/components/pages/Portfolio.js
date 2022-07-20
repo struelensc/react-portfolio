48147362b96a7a56ce57d28cb0e29460477c8c2b
Courtney Struelens <courtneystruelens@hotmail.com>
2022-07-19 22:28:32 -0700

    wired portfolio page

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,22 +1,91 @@
 import React from "react";
+import Project from "./page_components/Project";
+import "../../styles/portfolio.css";
+import github from "../../assets/Icons/github.png";
+import bens_diner from "../../assets/project-screenshots/bens_diner.png";
+import mvc_tech_blog from "../../assets/project-screenshots/mvc_tech_blog.png";
+import orm_backend from "../../assets/project-screenshots/orm_backend.png";
+import starparty from "../../assets/project-screenshots/starparty.png";
+import team_generator from "../../assets/project-screenshots/team_generator.png";
 
-export default function Work() {
+function Work() {
+  const projectInfo = [
+    {
+      name: "Tech Blog",
+      image: mvc_tech_blog,
+      github: "https://github.com/struelensc/mvc-tech-blog",
+      deployed: "https://mvc-tech-blog-09493.herokuapp.com/",
+    },
+    {
+      name: "StarParty",
+      image: starparty,
+      github: "https://github.com/millersg47/StarParty",
+      deployed: "https://millersg47.github.io/StarParty/",
+    },
+    {
+      name: "ORM Ecommerce Backend",
+      image: orm_backend,
+      github: "https://github.com/struelensc/orm-ecomm-backend",
+    },
+    {
+      name: "Team Profile Generator",
+      image: team_generator,
+      github: "https://github.com/struelensc/team-profile-generator",
+    },
+  ];
+
   return (
-    <div>
-      <h1>Portfolio Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
-    </div>
+    <section class="p-5" id="works">
+      <h2 class="py-3">What I've been working on</h2>
+
+      <div class="row d-flex flex-row-reverse">
+        <div class="col-lg-4 d-flex justify-content-center align-items-center">
+          <div class="container px-3">
+            <h3 class="d-block">
+              Ben's Diner
+              <span>
+                <a
+                  className="github_icon"
+                  href="https://github.com/wonjong2/Bens_Diner"
+                >
+                  <img src={github} alt="Github icon" />
+                </a>
+              </span>
+            </h3>
+
+            <p class="d-block">
+              Created a full stack web page application that allows restaurant
+              customers to view a digital menu, and order food without a human
+              server interface. Menu items are displayed and added to a shopping
+              cart on click, with routes navigating various button events. The
+              temporary shopping cart will save data using local storage. The
+              permanent order history will available in a MySQL database.
+            </p>
+          </div>
+        </div>
+
+        <figure class="col-lg-8 highlighted-project">
+          <a href="https://github.com/wonjong2/Bens_Diner" target="_blank">
+            <img
+              class="img"
+              src={bens_diner}
+              alt="Screenshot of Ben's Diner website"
+            />
+          </a>
+        </figure>
+      </div>
+
+      <div>
+        <h3 class="pt-5 px-5">Other projects</h3>
+
+        <div class="row">
+          {projectInfo.map((data, i) => (
+            <Project data={data} i={i}></Project>
+          ))}
+        </div>
+      </div>
+    </section>
   );
 }
+
+export default Work;
---
+++ src/components/pages/page_components/Project.js
@@ -0,0 +1,24 @@
+import React from "react";
+import github from "../../../assets/Icons/github.png";
+
+function Project({ data, i }) {
+  return (
+    <figure className="col-md-6" id={i}>
+      <img src={data.image} alt={"Screenshot of project"} />
+      <figcaption className="py-3 px-4 d-flex justify-content-between">
+        {data?.deployed ? (
+          <a href={data?.deployed} className="custom-card-text">
+            {data.name}
+          </a>
+        ) : (
+          <p className="m-0">{data.name}</p>
+        )}
+        <a className="github_icon" href={data.github}>
+          <img src={github} alt="Github icon" />
+        </a>
+      </figcaption>
+    </figure>
+  );
+}
+
+export default Project;
