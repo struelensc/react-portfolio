cb54e5fecdf02d7731ab769616ac79b05efd8593
Courtney Struelens <43919547+struelensc@users.noreply.github.com>
2023-02-06 15:54:54 -0800

    updated resume and removed unused code

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -2,7 +2,6 @@ import NavTabs from "./NavTabs";
 import About from "./sections/About";
 import Skills from "./sections/Skills";
 import Portfolio from "./sections/Portfolio";
-import Contact from "./sections/Contact";
 import Footer from "./Footer";
 
 export default function PortfolioContainer() {
