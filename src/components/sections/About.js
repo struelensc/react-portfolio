0e1d29f900432a814a89a098674a2f87bbdb12ed
Courtney Struelens <43919547+struelensc@users.noreply.github.com>
2023-03-30 13:35:24 -0700

    changed background color and added border styling

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -7,23 +7,40 @@ export default function About() {
   return (
     <section className="row py-5" id="about">
       <div className="d-flex flex-column-reverse flex-sm-row p-0 align-items-center">
-        <div className="col-sm-8 px-5 pt-5" id="aboutContent">
-          <h2>Hello!</h2>
+        <div className="aboutContainer col-sm-8 p-3">
+          <div className="d-flex px-3 row mr-3 pb-3">
+            <div className="col-11">
+              <div className="row my-2 horizontalLine"></div>
+              <div className="row my-2 horizontalLine"></div>
+              <div className="row my-2 horizontalLine"></div>
+              <div className="row my-2 horizontalLine"></div>
+              <div className="row my-2 horizontalLine"></div>
+            </div>
 
-          <p>
-            I am a <span className="fascinate">physics student</span> who holds
-            a certification of completion from University of Washington's{" "}
-            <span className="notable">Full Stack Web Development</span> boot
-            camp. I'm <span className="grandHotel">passionate</span> about the
-            work I do and strive to have a career filled with constant
-            opportunities to learn and grow. I enjoy helping people and I
-            believe there is something to{" "}
-            <span className="fascinate">
-              learn from everyone and every opportunity
-            </span>
-            .
-          </p>
-          <ContactList />
+            <div className="xBox col text-center">
+              <div>x</div>
+            </div>
+          </div>
+
+          <div className="px-3 py-3" id="aboutContent">
+            <h2>Hello!</h2>
+
+            <p>
+              I am a <span className="fascinate">physics student</span> who
+              holds a certification of completion from University of
+              Washington's{" "}
+              <span className="notable">Full Stack Web Development</span> boot
+              camp. I'm <span className="grandHotel">passionate</span> about the
+              work I do and strive to have a career filled with constant
+              opportunities to learn and grow. I enjoy helping people and I
+              believe there is something to{" "}
+              <span className="fascinate">
+                learn from everyone and every opportunity
+              </span>
+              .
+            </p>
+            <ContactList />
+          </div>
         </div>
 
         <div className="col-sm-4 px-4 pb-5 profilePhotoContainer">
