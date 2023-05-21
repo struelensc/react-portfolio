59dd2c88935d776a796fdf924a636eab6ce1b07c
Courtney Struelens <courtneystruelens@hotmail.com>
2023-05-21 11:48:54 -0700

    fixed full width scroll bar issue

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -5,46 +5,34 @@ import ContactList from "./page_components/ContactList";
 
 export default function About() {
   return (
-    <section className="row py-5" id="about">
-      <div className="d-flex flex-column-reverse flex-sm-row p-0 align-items-center">
-        <div className="aboutContainer col-sm-7 p-3 m-3">
-          <div className="d-flex px-3 row mr-3 pb-3">
-            <div className="col-11">
-              <div className="row my-2 horizontalLine"></div>
-              <div className="row my-2 horizontalLine"></div>
-              <div className="row my-2 horizontalLine"></div>
-              <div className="row my-2 horizontalLine"></div>
-              <div className="row my-2 horizontalLine"></div>
-            </div>
+    <section className="p-5" id="about">
+      <div className="row">
+        <div className="d-flex flex-column-reverse flex-sm-row p-0 align-items-center">
+          <div className="aboutContainer col-sm-7">
+            <div id="aboutContent">
+              <h2>Hello!</h2>
 
-            <div className="xBox col text-center">
-              <div>x</div>
+              <p>
+                I am a <span className="fascinate">physics student</span> who
+                holds a certification of completion from University of
+                Washington's{" "}
+                <span className="notable">Full Stack Web Development</span> boot
+                camp. I'm <span className="grandHotel">passionate</span> about
+                the work I do and strive to have a career filled with constant
+                opportunities to learn and grow. I enjoy helping people and I
+                believe there is something to{" "}
+                <span className="fascinate">
+                  learn from everyone and every opportunity
+                </span>
+                .
+              </p>
+              <ContactList />
             </div>
           </div>
 
-          <div className="px-3 py-3" id="aboutContent">
-            <h2>Hello!</h2>
-
-            <p>
-              I am a <span className="fascinate">physics student</span> who
-              holds a certification of completion from University of
-              Washington's{" "}
-              <span className="notable">Full Stack Web Development</span> boot
-              camp. I'm <span className="grandHotel">passionate</span> about the
-              work I do and strive to have a career filled with constant
-              opportunities to learn and grow. I enjoy helping people and I
-              believe there is something to{" "}
-              <span className="fascinate">
-                learn from everyone and every opportunity
-              </span>
-              .
-            </p>
-            <ContactList />
+          <div className="col-sm-5 profilePhotoContainer">
+            <img className="profilePhoto" alt="Photo of Courtney" src={photo} />
           </div>
-        </div>
-
-        <div className="col-sm-5 px-4 profilePhotoContainer">
-          <img className="profilePhoto" alt="Photo of Courtney" src={photo} />
         </div>
       </div>
     </section>
--- src/components/sections/Skills.js
+++ src/components/sections/Skills.js
@@ -5,53 +5,52 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Skills() {
   return (
-    <section
-      className="row py-5 d-flex flex-row-reverse align-items-center"
-      id="skills"
-    >
-      <div className="col-sm-7">
-        <h2 className="p-5">Some things I know</h2>
+    <section className="p-5" id="skills">
+      <div className="row d-flex flex-row-reverse align-items-center">
+        <div className="col-sm-7">
+          <h2 className="p-5">Some things I know</h2>
 
-        <div className="px-5">
-          <ul className="list-group list-group-horizontal flex-wrap">
-            <li className="text-center mx-4">
-              <FontAwesomeIcon icon="fa-brands fa-react" size="3x" />
-              <p>react</p>
-            </li>
-            <li className="text-center mx-4">
-              <FontAwesomeIcon icon="fa-brands fa-html5" size="3x" />
-              <p>html-5</p>
-            </li>
-            <li className="text-center mx-4">
-              <FontAwesomeIcon icon="fa-brands fa-css3-alt" size="3x" />
-              <p>css</p>
-            </li>
-            <li className="text-center mx-4">
-              <FontAwesomeIcon icon="fa-brands fa-bootstrap" size="3x" />
-              <p>bootstrap</p>
-            </li>
-            <li className="text-center mx-4">
-              <FontAwesomeIcon icon="fa-brands fa-js" size="3x" />
-              <p>javascript</p>
-            </li>
-            <li className="text-center mx-4">
-              <FontAwesomeIcon icon="fa-brands fa-node-js" size="3x" />
-              <p>node.js</p>
-            </li>
-            <li className="text-center mx-4">
-              <FontAwesomeIcon icon="fa-solid fa-database" size="3x" />
-              <p>sql-database</p>
-            </li>
-            <li className="text-center mx-4">
-              <FontAwesomeIcon icon="fa-brands fa-git" size="3x" />
-              <p>git</p>
-            </li>
-          </ul>
+          <div className="px-5">
+            <ul className="list-group list-group-horizontal flex-wrap">
+              <li className="text-center mx-4">
+                <FontAwesomeIcon icon="fa-brands fa-react" size="3x" />
+                <p>react</p>
+              </li>
+              <li className="text-center mx-4">
+                <FontAwesomeIcon icon="fa-brands fa-html5" size="3x" />
+                <p>html-5</p>
+              </li>
+              <li className="text-center mx-4">
+                <FontAwesomeIcon icon="fa-brands fa-css3-alt" size="3x" />
+                <p>css</p>
+              </li>
+              <li className="text-center mx-4">
+                <FontAwesomeIcon icon="fa-brands fa-bootstrap" size="3x" />
+                <p>bootstrap</p>
+              </li>
+              <li className="text-center mx-4">
+                <FontAwesomeIcon icon="fa-brands fa-js" size="3x" />
+                <p>javascript</p>
+              </li>
+              <li className="text-center mx-4">
+                <FontAwesomeIcon icon="fa-brands fa-node-js" size="3x" />
+                <p>node.js</p>
+              </li>
+              <li className="text-center mx-4">
+                <FontAwesomeIcon icon="fa-solid fa-database" size="3x" />
+                <p>sql-database</p>
+              </li>
+              <li className="text-center mx-4">
+                <FontAwesomeIcon icon="fa-brands fa-git" size="3x" />
+                <p>git</p>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
 
-      <div className="col-sm-5 vectorContainer">
-        <img src={skillsPhoto} alt="female programmer" />
+        <div className="col-sm-5 vectorContainer">
+          <img src={skillsPhoto} alt="female programmer" />
+        </div>
       </div>
     </section>
   );
