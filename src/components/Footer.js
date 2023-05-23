7bedd6a5d3efdaeabc5de883e067824f7e32400f
Courtney Struelens <courtneystruelens@hotmail.com>
2023-05-22 21:45:00 -0700

    updated footer styling and info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,42 @@
 import React from "react";
 import "../styles/footer.css";
 import ContactList from "./sections/page_components/ContactList";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Footer() {
   return (
-    <footer className="d-flex justify-content-center footer-extended">
-      <ContactList />
+    <footer className="footer-extended">
+      <div className="container pt-5 d-flex flex-column align-items-center justify-content-center">
+        <div className="py-4">
+          <h1>
+            <span className="tertiaryAccents">&lt; </span>
+            <span>CS</span>
+            <span className="tertiaryAccents"> /&gt;</span>
+          </h1>
+        </div>
+        <div className="py-5">
+          <h2>
+            Curiosity: the secret ingredient behind my caffeinated coding
+            sessions.
+          </h2>
+        </div>
+        <div className="py-5">
+          <ContactList />
+        </div>
+        <div className="py-5">
+          <h3 className="d-flex align-items-center">
+            Made with
+            <span>
+              <FontAwesomeIcon
+                className="px-3 icon"
+                icon="fa-brands fa-react"
+                size="2x"
+              />
+            </span>
+            React
+          </h3>
+        </div>
+      </div>
     </footer>
   );
 }
