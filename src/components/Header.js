0734caea1a419e955ac9813303c5547610a15f4f
Courtney Struelens <courtneystruelens@hotmail.com>
2023-06-04 17:51:56 -0700

    changed styling of header primarily for a better mobile design

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/header.css";
 import resume from "../files/Resume.pdf";
+import resumeIcon from "../assets/contact-icons/resume.png";
 
 function Header() {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    function handleResize() {
+      setIsSmallScreen(window.innerWidth < 768);
+    }
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <nav>
-      <div className="header-wrapper navbar justify-content-center justify-content-lg-between">
-        <h1 className="statement-text">
+      <div
+        className={`header-wrapper navbar justify-content-lg-between ${
+          isSmallScreen ? "justify-content-left" : ""
+        }`}
+      >
+        <h1 className={`statement-text`}>
           <span className="mainAccent">&lt; </span>
-          <span>courtney s</span>
+          <span>{isSmallScreen ? "cs" : "courtney s"}</span>
           <span className="mainAccent"> /&gt;</span>
         </h1>
 
-        <a href={resume} className="statement-text" target="_blank">
-          resume
+        <a className="icon" href={resume} target="_blank">
+          <img src={resumeIcon} alt="Resume icon" />
         </a>
       </div>
     </nav>
