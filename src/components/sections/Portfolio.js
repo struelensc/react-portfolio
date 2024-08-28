7e36c1b49ecac79729569cacc8a44f0d2ba8f5b7
Courtney Struelens <courtneystruelens@hotmail.com>
2024-08-27 20:02:25 -0700

    added compliment-bot project

--- src/components/sections/Portfolio.js
+++ src/components/sections/Portfolio.js
@@ -1,16 +1,25 @@
 import React from "react";
-import Project from "./page_components/Project";
 import "../../styles/portfolio.css";
 import bens_diner from "../../assets/project-screenshots/bens_diner.png";
 import mvc_tech_blog from "../../assets/project-screenshots/mvc_tech_blog.png";
 import rental_railroad_homepage from "../../assets/project-screenshots/rental_railroad_homepage.png";
 import google_scripts from "../../assets/project-screenshots/google_scripts.png";
+import compliment_bot from "../../assets/project-screenshots/compliment_bot.png";
 
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 function Work() {
   const projectInfo = [
+    {
+      name: "Compliment-Bot",
+      image: compliment_bot,
+      description:
+        "A Discord bot built with discord.js that uses slash commands to compliment users. This bot is designed to run on Heroku and register commands dynamically.",
+      tech: ["JavaScript", "discord.js"],
+      github: "https://github.com/struelensc/compliment-bot",
+      type: "Solo",
+    },
     {
       name: "Google Sheets Scripts",
       image: google_scripts,
@@ -36,16 +45,6 @@ function Work() {
       ],
       github: "https://github.com/rkutsel/rental-railroad",
       type: "Group",
-    },
-    {
-      name: "Tech Blog",
-      image: mvc_tech_blog,
-      description:
-        "This solo project was to build a CMS-style blog site where developers can publish their blog posts and comment on other developers’ posts as well. This app follows the MVC paradigm in its architectural structure, using Handlebars.js as the templating language, Sequelize as the ORM, and the express-session npm package for authentication.",
-      tech: ["Node.js", "Handlebars.js", "Sequelize", "Express.js", "MySQL"],
-      github: "https://github.com/struelensc/mvc-tech-blog",
-      deployed: "https://mvc-tech-blog-09493.herokuapp.com/",
-      type: "Solo",
     },
     {
       name: "Ben's Diner",
